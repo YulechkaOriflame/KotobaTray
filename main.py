93d71c4e50c81d94e5df60995513ab6a9a855e0b
ururu <restline2503@gmail.com>
2025-08-31 23:59:21 +0300

    Unidic path

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
+import sys
+from pathlib import Path
+
+def init_unidic():
+    if hasattr(sys, "_MEIPASS"):  # exe
+        base = Path(sys._MEIPASS)
+    else:  # dev
+        base = Path(__file__).resolve().parent
+    dic_path = base / "unidic"
+    os.environ["UNIDICDIR"] = str(dic_path)
+
+init_unidic()
+
 from app import app
 
 if __name__ == "__main__":
