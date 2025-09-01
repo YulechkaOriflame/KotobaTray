c3025b4eeb0d40719a15a91ad54f93e55e427791
ururu <restline2503@gmail.com>
2025-09-01 16:02:36 +0300

    Unidic path

--- app/utils/translate_and_format.py
+++ app/utils/translate_and_format.py
@@ -1,8 +1,20 @@
+import os
+import sys
+
 import cutlet
 import fugashi
 from deep_translator import GoogleTranslator
+from unidic import unidic
 
-tagger = fugashi.Tagger()
+def make_tagger():
+    if getattr(sys, "frozen", False):
+        base_path = os.path.dirname(sys.executable)
+        dic_path = os.path.join(base_path, "unidic")
+    else:
+        dic_path = unidic.DICDIR
+
+    return fugashi.Tagger(f'-d "{dic_path}"')
+tagger = make_tagger()
 
 def get_underlined_text(text: str) -> str:
     words = [word.surface for word in tagger(text)]
