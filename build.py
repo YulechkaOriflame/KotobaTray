8c19e36e7ba65539eb5d5b271bee2f3aeb543fe1
ururu <restline2503@gmail.com>
2025-09-01 14:51:57 +0300

    Unidic path

--- build.py
+++ build.py
@@ -8,4 +8,6 @@ PyInstaller.__main__.run([
     "--noconfirm",
     "--icon=tray-icon.ico",
     "--add-data=NotoSerifJP-VariableFont_wght.ttf;fonts",
+    "--add-data=unidic;unidic",
+    "--add-data=mecabrc;.",
 ])
